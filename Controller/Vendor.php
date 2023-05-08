76508bd77bc7bc24c7b4a843fbd6ef10bd267bc6
DhruvTilva <tilvadhruv8@gmail.com>
2023-05-08 17:39:29 +0530

    vendor commited with resource layout block

--- Controller/Vendor.php
+++ Controller/Vendor.php
@@ -2,17 +2,26 @@
 
 class Controller_Vendor extends Controller_Core_Action 
 {
+	public function render()
+		{
+			return $this->getView()->render();
+		}
 	
 	public function gridAction()
 	{
 		try {
-			$query = "SELECT * FROM `vendor`";
-			$vendors = Ccc::getModel('Vendor_Row')->fetchAll($query);
-			if (!$vendors) {
-				throw new Exception("Vendors not found", 1);
-			}
-			$this->getView()->setTemplate('vendor/grid.phtml')->setData(['vendors'=>$vendors]);
-			$this->render();
+			// $query = "SELECT * FROM `vendor`";
+			// $vendors = Ccc::getModel('Vendor_Row')->fetchAll($query);
+			// if (!$vendors) {
+			// 	throw new Exception("Vendors not found", 1);
+			// }
+			// $this->getView()->setTemplate('vendor/grid.phtml')->setData(['vendors'=>$vendors]);
+			// $this->render();
+			$layout = new Block_Core_Layout();
+			$grid = $layout->createBlock('Vendor_Grid');
+			$layout->getChild('content')->addChild('grid',$grid);
+			// echo $layout->toHtml();
+			$layout->render();
 		} catch (Exception $e) {
 			echo "catch found";
 		}
@@ -22,18 +31,58 @@ class Controller_Vendor extends Controller_Core_Action
 
 	public function addAction()
 	{
-		$this->getView()->setTemplate('vendor/add.phtml');
-		$this->render();
+		$message = Ccc::getModel('Core_Message');
+		try 
+		{
+			$vendor = Ccc::getModel('Vendor');
+			if(!$vendor){
+				throw new Exception("Invalid request.", 1);
+			}
+			
+			$layout = new Block_Core_Layout();
+			$edit = $layout->createBlock('Vendor_Edit');
+			$edit->setData(['vendor'=>$vendor]);
+			$layout->getChild('content')->addChild('edit',$edit);
+			echo $layout->toHtml();
+
+		} 
+		catch (Exception $e) 
+		{
+			$message->addMessage('Vendor not Saved.',Model_Core_Message::FAILURE);
+			$this->redirect('grid');
+		}
 	}
 
 	public function editAction()
 	{
-			$request = $this->getRequest();
-			$id = $request->getParams('id');
-			$vendor = Ccc::getModel('Vendor_Row')->load($id);
-			$vendorAddress = Ccc::getModel('Vendor_Address_Row')->load($id);
-			$this->getView()->setTemplate('vendor/edit.phtml')->setData(['vendor' => $vendor,'address' =>$vendorAddress]);
-			$this->render();
+			$message = Ccc::getModel('Core_Message');
+		try 
+		{
+			$id =$this->getRequest()->getParams('id');
+			if(!$id){
+	    		throw new Exception("Invalid request.", 1);
+			}
+			
+
+			$vendor=Ccc::getModel('Vendor')->load($id);
+			if(!$vendor){
+				throw new Exception("Invalid Id.", 1);
+			}
+
+			$layout = new Block_Core_Layout();
+			$edit = $layout->createBlock('Vendor_Edit');
+			$edit->setData(['vendor'=>$vendor]);
+			$layout->getChild('content')
+					->addChild('edit',$edit);
+			// $layout->render();
+			echo $layout->toHtml();
+
+		} 
+		catch (Exception $e) 
+		{
+			$message->addMessage('Vendor Not Saved',Model_Core_Message::FAILURE);
+			$this->redirect('grid');
+		}
 	}
 
 	public function saveAction()
@@ -49,12 +98,12 @@ class Controller_Vendor extends Controller_Core_Action
 			}
 			$id=$request->getParams('id');
 			if ($id) {
-				$vendor=Ccc::getModel('Vendor_Row')->load($id);
+				$vendor=Ccc::getModel('Vendor')->load($id);
 				date_default_timezone_set('Asia/Kolkata');
 				$vendor->updated_at=date('Y-m-d H:i:s');
 			}
 			else{
-				$vendor= Ccc::getModel('Vendor_Row');
+				$vendor= Ccc::getModel('Vendor');
 				date_default_timezone_set('Asia/Kolkata');
 				$vendor->inserted_at = date("Y-m-d h:i:s");
 			}
@@ -68,13 +117,13 @@ class Controller_Vendor extends Controller_Core_Action
 			}
 		
 			if ($id = (int)$this->getRequest()->getParams('id')) {
-			$vendorAddress = Ccc::getModel('Vendor_Address_Row')->load($id);
+			$vendorAddress = Ccc::getModel('Vendor_Address')->load($id);
 			if (!$vendorAddress) {
 				throw new Exception("Invalid id.", 1);
 			}
 		}
 		else{
-			$vendorAddress = Ccc::getModel('Vendor_Address_Row');
+			$vendorAddress = Ccc::getModel('Vendor_Address');
 			$vendorAddress->vendor_id = $vendor->vendor_id;
 		}
 			$vendorAddress->setData($postDataAddress);
@@ -89,12 +138,28 @@ class Controller_Vendor extends Controller_Core_Action
 
 	public function deleteAction()
 	{
-		$request = $this->getRequest();
-		$id = $request->getParams('id');
-		$query = "DELETE FROM `vendor` WHERE `vendor_id` = {$id}";
-		$adapter = $this->getAdapter();
-		$adapter->update($query);
-		header("Location:index.php?c=vendor&a=grid");
+		try
+		{
+		$message=Ccc::getModel('Core_Message');
+		$request=$this->getRequest();
+		$id = (int) $request->getParams('id');
+		if(!$id){
+			throw new Exception("Invalid ID.", 1);
+		}
+		$vendorModeRow = Ccc::getModel('Vendor'); 
+		$vendorModeRow->load($id);
+		$vendorResult = $vendorModeRow->delete();
+		if(!$vendorResult)
+		{
+			throw new Exception("Error Data is Not Deleted", 1);
+		}
+		$message->addMessage('Vendor Deleted Successfully',Model_Core_Message::SUCCESS);
+		}
+		catch(Exception $e)
+		{
+			$message->addMessage('Vendor is Not Deleted',Model_Core_Message::FAILURE);
+		}
+		$this->redirect('grid');
 	}
 }
 ?>
